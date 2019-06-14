19de42f7a654c8d1e7ddc576056142a2eeecc43b
jin <jinliansheng@163.com>
2019-06-14 13:42:54 +0800

    修改

--- src/main/java/com/example/sqlserver/controller/UserController.java
+++ src/main/java/com/example/sqlserver/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.sqlserver.controller;
 
 import com.example.sqlserver.domain.User;
 import com.example.sqlserver.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,26 +10,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/sqlServer")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/getAllUser", method = RequestMethod.GET)
+
+    @ApiOperation(value = "查询所有", notes = "所有信息")
+    @RequestMapping(value = "/getAllUser")
     public List<User> getAllUser() {
-        return userService.getAllUsers();
+        List<User> users = userService.getAllUsers();
+        return users;
     }
 
+    @ApiOperation(value = "添加用户", notes = "添加用户")
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
-    public int addUser(@RequestBody User user) {
+    public boolean addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
+    @ApiOperation(value = "删除用户", notes = "删除用户")
     @RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
-    public int deleteUser(@RequestBody User user) {
+    public boolean deleteUser(@RequestBody User user) {
         return userService.deleteUser(user);
+    }
+
+
+    @ApiOperation(value = "更新用户", notes = "更新用户")
+    @RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+    public boolean updateUser(@RequestBody User user) {
+        System.out.println("+++++++----------"+user.getSex());
+        return userService.updatePerson(user);
     }
 
 
