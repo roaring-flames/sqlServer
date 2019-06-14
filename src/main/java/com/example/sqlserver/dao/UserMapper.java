3eace2d36f5da5569fce66ccc2247dfa52296089
jin <jinliansheng@163.com>
2019-06-14 13:43:58 +0800

    修改

--- src/main/java/com/example/sqlserver/dao/UserMapper.java
+++ src/main/java/com/example/sqlserver/dao/UserMapper.java
@@ -2,12 +2,20 @@ package com.example.sqlserver.dao;
 
 import com.example.sqlserver.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface UserMapper {
+
     List<User> getAllUsers();
-    int addUser( User user );
-    int deleteUser( User user );
+
+    boolean addUser(User user);
+
+    boolean deleteUser(User user);
+
+    boolean updatePerson(User user);
+
 }
